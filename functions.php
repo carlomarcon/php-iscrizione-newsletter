ed02357e4de9fdfd4cd6e96ea7f8aa65a9a7fc93
carlomarcon <carlomarcon1998@gmail.com>
2023-12-13 19:48:14 +0100

    ADD: CLASS

--- functions.php
+++ functions.php
@@ -1,15 +1,17 @@
 <?php 
+
+
 function check($item) {
 //    var_dump($item);
 //    echo $item[1];
 
 // for ($i=0;$i<strlen($item);$i++){
 if(str_contains($item,'@')&&str_contains($item,'.')){
-  
+return true;
 }
 else{
-  
+return false;
 }
-};
+}
 // }
 ?>
--- index.php
+++ index.php
@@ -3,25 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/styles.css">
     <title>Document</title>
 </head>
 <body>
    <form action="index.php" method="GET">
     <label for="email">
-        <input name="email" type="text" >
+        <input name="email" type="text" class="<?php 
+          include_once __DIR__.'/functions.php';
+        
+        if(isset($_GET['email'])) {if(check($_GET['email'])==true){echo "success"; } else {}} 
+        ?>" >
     </label>
-    <button class="submit">Invia</button>
+    <button class="submit">Invia </button>
    </form>
+   
 
-
-   <?php
+   <!-- <?php
    include_once __DIR__.'/functions.php';
    
-   if(isset($_GET['email'])) {
-    echo check($_GET['email']);
-   } 
+//    if(isset($_GET['email'])) {
+//     echo 'ok';
+//    } 
 
-   ?>
+   ?> -->
 
    
 </body>
