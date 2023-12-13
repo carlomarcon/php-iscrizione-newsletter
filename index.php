beb0c81faeaf2044156368ae5b9de2a0d367edb5
carlomarcon <carlomarcon1998@gmail.com>
2023-12-13 22:34:10 +0100

    ADD: REDIRECTING

--- index.php
+++ index.php
@@ -4,30 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
+  <div class="wrapper d-flex w-100 justify-content-center align-items-center">
+  <div class="container w-25">
    <form action="index.php" method="GET">
     <label for="email">
-        <input name="email" type="text" class=
+        <input name="email" type="text" placeholder=
+        <?php 
+         include_once __DIR__.'/functions.php';
+        if(isset($_GET['email'])) {
+          if (check($_GET['email'])==true) {echo "'Email corretta'";} else {echo "'EMAIL ERRATA'";} }else{echo 'Scrivere email qui';}?>
+        
+        
+         class=
         "<?php 
           include_once __DIR__.'/functions.php';
         
-        if(isset($_GET['email'])) {if(check($_GET['email'])==true){echo "success"; } else {echo "failure";}} 
+        if(isset($_GET['email'])) {if(check($_GET['email'])==true){echo "success"; header('Location: ./thanks.php'); } else {echo "failure";}} else{echo 'normal';} 
         ?>" >
     </label>
     <button class="submit">Invia </button>
    </form>
+
+   </div>
+   </div>
    
 
-   <!-- <?php
-   include_once __DIR__.'/functions.php';
-   
-//    if(isset($_GET['email'])) {
-//     echo 'ok';
-//    } 
 
-   ?> -->
+   
+
 
    
 </body>
